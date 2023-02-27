f84b0203adc6fd304016a46ebdeac41e28c10cf0
Hossain2024 <95951583+Hossain2024@users.noreply.github.com>
2023-02-27 13:42:46 -0800

    commit main

--- Main.java
+++ Main.java
@@ -11,17 +11,8 @@ public class Main {
     Function<CellToken, Integer> getCellToken = (cell) -> {
       return 0;
     };
-<<<<<<< HEAD
-    
-    
-    Cell cell = new Cell(1, 1, "5(5+1)", getCellToken);
 
-    
-    
-   
-=======
     Cell cell = new Cell(1, 1, "5*(5-1*5/2)*3", getCellToken);
->>>>>>> branch 'master' of https://github.com/RosesAreMerry/SpreadsheetApp.git
     System.out.println(cell.getValue());
     System.out.println(cell.getFormula());
     
