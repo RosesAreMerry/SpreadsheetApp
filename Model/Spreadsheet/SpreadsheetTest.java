9711bd13a23bbf88ae241d77d4306e21a8640482
Louis Lomboy <llomboy@uw.edu>
2023-02-27 18:56:11 -0800

    fixed some errors

---
+++ Model/Spreadsheet/SpreadsheetTest.java
@@ -0,0 +1,47 @@
+package Model.Spreadsheet;
+
+import Spreadsheet.Spreadsheet;
+import Cell.Cell;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpreadsheetTest {
+    Spreadsheet spreadsheet = new Spreadsheet(4);
+    Cell myCell = new Cell(0, 0, 0, 0);
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void printValues() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void printCellFormula() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void printAllFormulas() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void changeCellFormula() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void changeCellFormulaAndRecalculate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testChangeCellFormulaAndRecalculate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNumRows() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNumColumns() {
+    }
+}
