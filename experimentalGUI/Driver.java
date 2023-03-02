1a6e8e18bcb80eec9ed7a159d04b3bf5de54f2be
jae253 <jae253@uw.edu>
2023-03-02 01:06:07 -0800

    3/1/23
    Added "experimental GUI" package, GUI and driver classes to showcase
    construction of primitive and as-of-yet non-functioning GUI

---
+++ experimentalGUI/Driver.java
@@ -0,0 +1,37 @@
+/*
+ * TCSS 305 - Assignment 4
+ */
+
+package experimentalGUI;
+
+import java.awt.EventQueue;
+
+/**
+ * Runs the program by instantiating and starting the GUI.
+ * 
+ * @author TCSS 305 instructors
+ * @version 1.3
+ */
+public final class Driver {
+
+    /**
+     * Private constructor, to prevent instantiation of this class.
+     */
+    private Driver() {
+        throw new IllegalStateException();
+    }
+
+    /**
+     * The main method, invokes the GUI. Command line arguments are ignored.
+     * 
+     * @param theArgs Command line arguments.
+     */
+    public static void main(final String[] theArgs) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new GUI();
+            }
+        });
+    }
+}
